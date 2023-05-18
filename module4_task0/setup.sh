7ce1f9e4b38d55459441552802a901fc87404e82
soOwasTaken <444deph12@gmail.com>
2023-05-18 13:33:59 +0000

    testing..

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,23 +1,2 @@
 #!/bin/bash
-
-set -e
-
-# Install required packages
-apt-get update && apt-get install -y wget make zip unzip
-
-# Download Hugo v0.84.0 extended
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
-
-# Extract and install Hugo
-tar -xf hugo_extended_0.84.0_Linux-64bit.tar.gz
-mv hugo /usr/local/bin/
-rm -rf hugo_extended_0.84.0_Linux-64bit.tar.gz LICENSE README.md
-
-# install golangci-lint && markdown-link-check
-npm install -g markdownlint-cli@0.26.0
-npm install -g markdown-link-check@3.8.6
-markdownlint --version
-
-
-# Go to the project directory
-cd "$(dirname "$0")"
+apt-get update && apt-get install -y git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb
